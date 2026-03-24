6a7b4371448281ffc1592faf05148adaae80b5f1
Ayesha Dawodi <166828984+dlawiz83@users.noreply.github.com>
2026-03-24 10:35:02 +0430

    fix: show generate button when recap is from previous week

--- src/pages/RecapsPage.tsx
+++ src/pages/RecapsPage.tsx
@@ -242,7 +242,7 @@ const RecapsPage = () => {
                 />
               ))}
             </div>
-          ) : !recap ? (
+          ) : !recap || !recapAlreadyExists ? (
             <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
